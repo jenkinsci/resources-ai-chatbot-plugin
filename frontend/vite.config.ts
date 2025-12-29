0db7b2387d90be773b83ac67a96de82a52bcf3a6
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-12-29 22:58:57 +0530

    fix: forntend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const outDir = (process.env as { DOCKER_BUILD?: string }).DOCKER_BUILD
+  ? "dist"
+  : "../src/main/webapp/static";
+
 export default defineConfig({
   plugins: [react()],
   base: "",
   build: {
-    outDir: "dist",
+    outDir: outDir,
     assetsDir: "assets",
     rollupOptions: {
       output: {
