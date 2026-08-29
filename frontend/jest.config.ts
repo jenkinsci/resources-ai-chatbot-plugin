1366b97920fb523b12b43815a4b71983bab0cd41
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-08-29 11:56:35 +0700

    feat: Upgrade eslint, typescript, etc and use babel instead of ts-jest
    Signed-off-by: Bervianto Leo Pratama <bervianto.leo@gmail.com>

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,9 +3,6 @@ import type { Config } from "jest";
 const config: Config = {
   verbose: true,
   testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
   setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
 };
 
